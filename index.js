6cc0855eda4ce3684770bc35b9675bf90af98469
Twenmod <Twengames@gmail.com>
2024-04-18 00:20:35 +0200

    aaa

--- index.js
+++ index.js
@@ -9,26 +9,22 @@ function fullFullscreen() {
 
 }
 
-//setInterval(setFullscreen, 100);
-//setInterval(fullFullscreen, 100);
+setInterval(setFullscreen, 100);
+setInterval(fullFullscreen, 100);
 
-function annoyance() {
-    document.moveTo(Math.floor(Math.random() * screen.width), Math.floor(Math.random() * screen.height));
+function annoyance(windowtouse) {
+    windowtouse.moveTo(Math.floor(Math.random() * screen.width), Math.floor(Math.random() * screen.height));
+    windowtouse.focus();
 }
-
-setInterval(annoyance, 50);
-
-/*
 function popup(url) {
     for (var I = 0; I < 5; I++) {
       var i = Math.random();
-      newwindow = window.open(url, i, 'height=170,width=800');
+      newwindow = window.open(url, i, 'height=1920,width=1680');
       if (window.focus) { newwindow.focus() }
       setInterval(annoyance, 50, newwindow);
-  
-  
+
+
     }
     return false;
   }
-  popup("https://twenmod.github.io/Site/");
-*/
+  popup("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
