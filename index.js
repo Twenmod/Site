b5e6b3c26190559afc612aea385bd20ab42d30b0
Twenmod <Twengames@gmail.com>
2024-04-17 23:48:15 +0200

    no crash

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var elem = document.getElementById("rick");
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 while (true) {
     elem.requestFullscreen();
+    //Sleep
+    await sleep(100);
 }
